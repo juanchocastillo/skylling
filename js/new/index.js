a98b237396b7128c2766a45ebb5663734b52e63c
jesus <jesuseduardocast18@gmail.com>
2023-05-26 23:46:31 -0400

    dev question-section: js

--- js/new/index.js
+++ js/new/index.js
@@ -1,63 +1,7 @@
 
 
-let handlerAnimation=(elem, animation)=>{
-        
-    if(elem.getAttribute("clicked")=="false"){
-        
-        elem.animate(animation["init"], animation["config"]);
-        elem.setAttribute("clicked", "true");
-    }
+/*test(".question-title__icon-arrow", [".question"],
 
-    else{
-
-        elem.animate(animation["close"], animation["config"]);
-        elem.setAttribute("clicked", "false");
-    }
-}
-
-
-
-let test=(classElement, classNodes, animations)=>{
-
-    
-   /* let nodesToAnimate=[]*/
-    
-    let nodeListToAdd=[];
-    classNodes.map((elem)=>{nodeListToAdd.push(document.querySelectorAll(elem))})
-    
-
-
-    const elements=Array.from(document.querySelectorAll(classElement));
-
-
-    elements.map((elem, index)=>{
-
-       elem.addEventListener("click", ()=>{
-            
-                handlerAnimation(elem,animations.buttonAnimation)
-                
-
-            for(let i=0; i<nodeListToAdd.length; i++){
-                   
-                handlerAnimation(nodeListToAdd[i][index], animations.animationAccion)
-              
-            }
-    
-    
-        })
-
-
-
-    })
-    
-}   
-
-
-
-
-
-
-test(".question-title__icon-arrow", [".question"],
     {
         buttonAnimation:{
             init:[{transform:"rotate(-180deg)"},{transform:"rotate(-90deg)"} ],
@@ -72,7 +16,135 @@ test(".question-title__icon-arrow", [".question"],
         }
     }
 
- )
+ )*/
+
+ /*BOTONES*/
+
+ class animationButton{
+
+    constructor(){
+        this.handlerAnimation=(elem, animation)=>{
+        
+            if(elem.getAttribute("clicked")=="false"){
+                
+                elem.animate(animation["init"], animation["config"]);
+                elem.setAttribute("clicked", "true");
+            }
+        
+            else{
+        
+                elem.animate(animation["close"], animation["config"]);
+                elem.setAttribute("clicked", "false");
+            }
+        }
+        
+    }
+
+    
+    deploy=(classElement, classNodes, animations)=>{
+        
+        let nodeListToAdd=[];
+        classNodes.map((elem)=>{nodeListToAdd.push(document.querySelectorAll(elem))})
+        
+    
+    
+        const elements=Array.from(document.querySelectorAll(classElement));
+    
+    
+        elements.map((elem, index)=>{
+    
+            elem.addEventListener("click", ()=>{
+                
+                   this.handlerAnimation(elem,animations.buttonAnimation)
+                    
+    
+                for(let i=0; i<nodeListToAdd.length; i++){
+                        
+                   this.handlerAnimation(nodeListToAdd[i][index], animations.animationAccion)
+                
+                }
+        
+        
+            })
+    
+    
+    
+        })
+        
+    }   
+
+
+ }  
+
+
+/*Arrow*/
+
+
+ let arrowsButton=(button, elementToXpand, sizeTex)=>{
+
+    
+    const xpandSize=Array.from(document.querySelectorAll(sizeTex));
+    const arrows=Array.from(document.querySelectorAll(button));
+    const xpand=Array.from(document.querySelectorAll(elementToXpand));
 
 
 
+    arrows.map((elem, index)=>{
+
+        elem.style.transition="All 0.2s"
+        xpand[index].style.transition="All 0.2s ease"
+    
+        elem.addEventListener("click", ()=>{
+              
+               
+
+
+            if(elem.getAttribute("clicked")=="false"){
+                
+                arrows.map((elem, index)=>{
+                    
+                    
+                    if(elem.getAttribute("clicked")=="true"){
+                        console.log("si")
+
+                        elem.style.transform="rotate(-180deg)";
+                        xpand[index].style.height=`60px`
+
+                        elem.setAttribute("clicked", "false");
+                        xpand[index].setAttribute("clicked", "false");
+                    
+                    }
+
+                    
+                })
+
+
+
+                elem.style.transform="rotate(-90deg)"
+                xpand[index].style.height=`${xpandSize[index].offsetHeight+xpand[index].offsetHeight}px`
+               
+                console.log(xpandSize[index].offsetHeight)
+
+
+                elem.setAttribute("clicked", "true");
+                xpand[index].setAttribute("clicked", "true");
+
+            }
+        
+            else{
+        
+                elem.style.transform="rotate(-180deg)"
+                xpand[index].style.height=`60px`
+                elem.setAttribute("clicked", "false");
+                xpand[index].setAttribute("clicked", "false");
+            }
+    
+        })
+
+    })
+
+ }
+
+
+
+ arrowsButton(".question-title__icon-arrow", ".question", ".question-text");
